4804428656e0e6b3b8ebd282c9b27711ed657108
Hongbin Lu <hongbin034@gmail.com>
2018-04-18 03:22:50 +0000

    Re-enable rename container test
    This test was disabled because the API endpoint of rename was
    removed and re-introduced by the 'patch' endpoint. The change
    is finished so this patch re-enable the test with the new
    API endpoint.
    
    Change-Id: I1789822e34452ba90bfb15eb91e34aa262dd0f88

--- zun_tempest_plugin/tests/tempest/api/test_containers.py
+++ zun_tempest_plugin/tests/tempest/api/test_containers.py
@@ -449,7 +449,6 @@ class TestContainer(base.BaseZunTest):
                                                      docker_base_url)
         self._assert_resource_constraints(container, cpu=0.2, memory=200)
 
-    @decorators.skip_because(bug="1759459")
     @decorators.idempotent_id('b218bea7-f19b-499f-9819-c7021ffc59f4')
     def test_rename_container(self):
         container1_name = data_utils.rand_name('container1')
@@ -457,7 +456,7 @@ class TestContainer(base.BaseZunTest):
         self.assertEqual(container1_name, model.name)
         container2_name = data_utils.rand_name('container2')
         gen_model = datagen.container_rename_data(name=container2_name)
-        resp, model = self.container_client.rename_container(model.uuid,
+        resp, model = self.container_client.update_container(model.uuid,
                                                              gen_model)
         self.assertEqual(200, resp.status)
         self.assertEqual(container2_name, model.name)
